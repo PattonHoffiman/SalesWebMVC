54f2ad7d8da5a2b1652bf1f8a646165a95aae8fa
Patton Hoffiman <PattonHoffiman@gmail.com>
2019-11-24 01:16:40 -0300

    Validation

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -8,16 +8,24 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} Size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
+        [EmailAddress(ErrorMessage = "Enter a Valid Email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [DataType(DataType.Date)]
         [Display(Name = "Birth Date")]
         public DateTime BirthDate { get; set; }
-        
+
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
+        [Required(ErrorMessage = "{0} Required")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         [Display(Name = "Base Salary")]
         public double BaseSalary { get; set; }
